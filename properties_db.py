d7727cb2d93a391b2ce22002e2df208715854df0
hicham <hicham.alaoui@leedscitycollege>
2018-01-05 22:00:41 +0000

    properties app
    update git repo

---
+++ properties_db.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Area(Base):
+    __tablename__ = 'area'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+           'name': self.name,
+           'id': self.id,
+        }
+
+
+class Property(Base):
+    __tablename__ = 'properties'
+    address = Column(String(300), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(450))
+    city = Column(String(40))
+    price = Column(String(16))
+    area_id = Column(Integer, ForeignKey('area.id'))
+    area = relationship('Area')
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+           'address': self.address,
+           'description': self.description,
+           'id': self.id,
+           'city': self.city,
+        }
+
+engine = create_engine('sqlite:///properties_list.db')
+
+Base.metadata.create_all(engine)
